d1b2a5c5d7ebb188be24725021a6f3b65ad891f9
Sidwell Nkosi <14979632@students.wits.ac.za>
2019-04-17 23:16:35 +0300

    fourth commit

--- mainRoutes.js
+++ mainRoutes.js
@@ -2,12 +2,12 @@ let path = require('path')
 let express = require('express')
 let mainRouter = express.Router()
 
-// mainRouter.get('/', function (req, res) {
-//   res.send('Hello new World')
-// })
-
-mainRouter.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+mainRouter.get('/', function (req, res) {
+  res.send('Sydwell of house Nkosi first of his name')
 })
+
+// mainRouter.get('/about', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'views', 'about.html'))
+// })
 
 module.exports = mainRouter
